dfe48cc6c5e14abea62f2509075d1dd023076992
christian.o.hockey@gmail.com <43612425+chockeyblocky@users.noreply.github.com>
2023-10-22 16:21:09 +0100

    Fixed einsum, added weights and constrained kernel to be made of rotors.

--- layers/layers.py
+++ layers/layers.py
@@ -16,7 +16,7 @@ from tfga.tfga import GeometricAlgebra
 class RotorConv1D(GeometricAlgebraLayer):
     """
     This is a convolution layer as described in "Geometric Clifford Algebra
-    Networks". It uses a weighted sandwich product with rotors in the kernel.
+    Networks" (Ruhe et al.). It uses a weighted sandwich product with rotors in the kernel.
 
 
      Args:
@@ -37,7 +37,7 @@ class RotorConv1D(GeometricAlgebraLayer):
             kernel_size: int,
             stride: int,
             padding: str,
-            blade_indices_kernel: List[int],
+            blade_indices_kernel: List[int] = None,
             blade_indices_bias: Union[None, List[int]] = None,
             dilations: Union[None, int] = None,
             activation=None,
@@ -47,7 +47,7 @@ class RotorConv1D(GeometricAlgebraLayer):
             kernel_regularizer=None,
             bias_regularizer=None,
             activity_regularizer=None,
-            kernel_constraint=None,
+            kernel_constraint=tf.keras.constraints.UnitNorm(axis=-1),
             bias_constraint=None,
             **kwargs
     ):
@@ -60,6 +60,10 @@ class RotorConv1D(GeometricAlgebraLayer):
         self.stride = stride
         self.padding = padding
         self.dilations = dilations
+
+        # if no blade index specified, default to rotors (only even indices)
+        if blade_indices_kernel is not None:
+            blade_indices_kernel = self.algebra.get_kind_blade_indices('even')
 
         self.blade_indices_kernel = tf.convert_to_tensor(
             blade_indices_kernel, dtype_hint=tf.int64
@@ -98,6 +102,13 @@ class RotorConv1D(GeometricAlgebraLayer):
             dtype=self.dtype,
             trainable=True,
         )
+        self.kernel_weights = self.add_weight(
+            "kernel_weights",
+            shape=shape_kernel[:-1],
+            regularizer=self.kernel_regularizer,
+            dtype=self.dtype,
+            trainable=True
+        )
         if self.use_bias:
             shape_bias = [self.filters, self.blade_indices_bias.shape[0]]
             self.bias = self.add_weight(
@@ -117,6 +128,7 @@ class RotorConv1D(GeometricAlgebraLayer):
             self,
             a_blade_values: tf.Tensor,
             k_blade_values: tf.Tensor,
+            weights,
             stride: int,
             padding: str,
             dilations: Union[int, None] = None,
@@ -165,21 +177,24 @@ class RotorConv1D(GeometricAlgebraLayer):
 
         # a_...p,k,ci,bi; k_k,ci,co,bk; c_bi,bk,bo -> y_...p,co,bo
         #   ...a b c  d ,   e c  f  g ,   d  g  h  ->   ...a f  h
-        x = tf.einsum("...abci,hij,...abcd,bcfg,dgh->...afj", self.algebra.reversion(a_slices), self.algebra._cayley,
-                      a_slices, k_blade_values, self.algebra._cayley)
-        # this works to implement sandwich product; add ...abc indices for weights if weighted kernel desired
+        x = tf.einsum("...bcf,...bcfi,hij,...abcd,bcfg,dgh->...afj", weights, self.algebra.reversion(k_blade_values),
+                      self.algebra._cayley, a_slices, k_blade_values, self.algebra._cayley)
 
         return x
 
     def call(self, inputs):
         k_geom = self.algebra.from_tensor(self.kernel, self.blade_indices_kernel)
 
+        print(self.kernel.shape)
+        print(self.kernel_weights.shape)
         inputs = tf.convert_to_tensor(inputs, dtype_hint=tf.float32)
         k_geom = tf.convert_to_tensor(k_geom, dtype_hint=tf.float32)
+        weights = tf.convert_to_tensor(self.kernel_weights, dtype_hint=tf.float32)
 
         result = self.rotor_conv1d(
             inputs,
             k_geom,
+            weights,
             stride=self.stride,
             padding=self.padding,
             dilations=self.dilations,
