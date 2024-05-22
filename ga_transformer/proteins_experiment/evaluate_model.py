073caf27b450adf68d4773742d84b3a3d789e769
christian.o.hockey@gmail.com <43612425+chockeyblocky@users.noreply.github.com>
2024-05-22 21:05:04 +0100

    Added histogram of results plotting to model evaluation.

--- ga_transformer/proteins_experiment/evaluate_model.py
+++ ga_transformer/proteins_experiment/evaluate_model.py
@@ -32,7 +32,33 @@ def load_weights(model_name, model):
     model.set_weights(weights)
 
 
-def evaluate(model, filenames_list, features_path, distances_path, coords_path):
+def save_statistics(stat_name, data):
+    """
+    Saves computed model evaluation statistics for plotting.
+    :param stat_name: name under which to save stats
+    :param data: tuple containing evaluated information
+    :return:
+    """
+
+    path = PATH + "model_histories/{}.pkl".format(stat_name)
+    with open(path, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def load_statistics(stat_name):
+    """
+    Loads computed statistics for plotting.
+    :param stat_name: name under which stats are saved
+    :return: (mse_list, mae_list)
+    """
+    path = PATH + "model_histories/{}.pkl".format(stat_name)
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+
+    return data
+
+
+def evaluate(model, filenames_list, features_path, distances_path, coords_path, stat_name):
     """
     Finds and outputs mae and ssim for a trained model.
     :param model: model to assess
@@ -40,12 +66,16 @@ def evaluate(model, filenames_list, features_path, distances_path, coords_path):
     :param features_path: path to features
     :param distances_path: path to distances
     :param coords_path: path to coordinates
+    :param stat_name: name under which to save statistics
     :return:
     """
     avg_mae = 0
     avg_ssim = 0
     avg_mse = 0
     count = 0
+
+    mse_list = []
+    mae_list = []
 
     mae = tf.keras.losses.MeanAbsoluteError()
     mse = tf.keras.losses.MeanSquaredError()
@@ -66,7 +96,8 @@ def evaluate(model, filenames_list, features_path, distances_path, coords_path):
         oriented_coords = orient_coords(coord, true_coords)
 
         # compute mse between oriented and true coordinates
-        avg_mse += tf.reduce_mean(mse(oriented_coords, true_coords))
+        mse_list.append(tf.reduce_mean(mse(oriented_coords, true_coords)).numpy())
+        avg_mse += mse_list[-1]
 
         # convert to predicted distances
         coord = tf.repeat(coord, repeats=coord.shape[1], axis=0)
@@ -76,8 +107,9 @@ def evaluate(model, filenames_list, features_path, distances_path, coords_path):
         # compute loss
         actual_dist = tf.expand_dims(tf.expand_dims(tf.cast(distance, tf.float32), -1), 0)
 
-        avg_mae += tf.reduce_mean(mae(pred_dist, actual_dist))
-        avg_ssim += tf.image.ssim(img1=pred_dist, img2=actual_dist, max_val=255)
+        mae_list.append(tf.reduce_mean(mae(pred_dist, actual_dist)).numpy())
+        avg_mae += mae_list[-1]
+        avg_ssim += tf.image.ssim(img1=pred_dist, img2=actual_dist, max_val=255).numpy()
 
         if count % 20 == 0:
             print(count, "/", len(filenames_list))
@@ -92,6 +124,8 @@ def evaluate(model, filenames_list, features_path, distances_path, coords_path):
     print("MAE:", avg_mae)
     print("Avg SSIM:", avg_ssim)
     print("MSE:", avg_mse)
+
+    save_statistics(stat_name, (mse_list, mae_list))
 
 
 def visualise_output(cga_model, mlp, filename, features_path, distances_path, coords_path):
@@ -124,7 +158,7 @@ def visualise_output(cga_model, mlp, filename, features_path, distances_path, co
     mlp_np = tf.reshape(mlp_oriented_coords, [-1, 3]).numpy()
 
     # plot coordinates
-    fig = plt.figure(figsize=(12, 12))
+    fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(projection='3d')
 
     ax.scatter(actual_np[:, 0], actual_np[:, 1], actual_np[:, 2], c='b')
@@ -135,6 +169,42 @@ def visualise_output(cga_model, mlp, filename, features_path, distances_path, co
     ax.plot(mlp_np[:, 0], mlp_np[:, 1], mlp_np[:, 2], c='g', label="MLP predicted")
 
     ax.legend()
+    plt.title("Plot of predicted and actual protein structure")
+    ax.set_ylabel("y")
+    ax.set_xlabel("x")
+    ax.set_zlabel("z")
+    plt.show()
+
+
+def load_mse_mae(model_name):
+    """
+    Loads mse and mae lists for a given model
+    :param model_name: name of model to load statistics of
+    :return: train, validation and test stats
+    """
+
+    train = load_statistics(model_name + "_train")
+    val = load_statistics(model_name + "_val")
+    test = load_statistics(model_name + "_test")
+
+    return (train, val, test)
+
+
+def plot_histogram(stats, metric='MSE', blocks=1):
+    """
+    Plots a histogram of given summary statistics.
+    :param stats: array for which to plot the histogram
+    :param metric: name of metric being plotted
+    :param blocks: number of blocks used in projector
+    :return:
+    """
+    bins = np.linspace(0, 100, 100)
+    plt.figure(figsize=(8, 4))
+    plt.hist(stats, bins, rwidth=1)
+    plt.ylim((0, 450))
+    plt.xlabel(metric)
+    plt.ylabel('Number of occurrences')
+    plt.title('Histogram of {} for {}-block CGATr projector'.format(metric, str(blocks)))
     plt.show()
 
 
@@ -146,7 +216,25 @@ def main():
 
     model = cga_transformer_model(num_blocks=2)
     mlp = mlp_model()
-    load_weights("psp_cgatr_2_block", model)  # demonstrates loading of model
+    model_name = "psp_cgatr_2_block"
+
+    # lst = load_mse_mae(model_name)
+    #
+    # for t in lst[:-2]:
+    #     print("///////////")
+    #     print(sorted(t[0])[-20:])
+    #     plot_histogram(t[0], 'MSE', 2)
+    #
+    # model_name = "psp_cgatr_1_block"  # "psp_cgatr_1_block"  # "psp_cgatr_new_5e-4_2_block"
+    #
+    # lst = load_mse_mae(model_name)
+    #
+    # for t in lst[:-2]:
+    #     print("///////////")
+    #     print(sorted(t[0])[-20:])
+    #     plot_histogram(t[0], 'MSE', 1)
+
+    load_weights(model_name, model)  # demonstrates loading of model
     load_weights("psp_mlp", mlp)
 
     deepcov_features_path = DATA_PATH + '/data/deepcov/features/'
@@ -181,9 +269,11 @@ def main():
     visualise_output(model, mlp, lsttrain[2], deepcov_features_path, deepcov_distances_path, deepcov_coords_path)
     del mlp
 
-    evaluate(model, lstval, deepcov_features_path, deepcov_distances_path, deepcov_coords_path)
-    evaluate(model, lst_test, psicov_features_path, psicov_distances_path, psicov_coords_path)
-    evaluate(model, lsttrain, deepcov_features_path, deepcov_distances_path, deepcov_coords_path)
+    evaluate(model, lst_test, psicov_features_path, psicov_distances_path, psicov_coords_path, model_name + "_test")
+    evaluate(model, lstval, deepcov_features_path, deepcov_distances_path, deepcov_coords_path, model_name + "_val")
+    evaluate(model, lsttrain, deepcov_features_path, deepcov_distances_path, deepcov_coords_path, model_name +
+    "_train")
+
 
 
 if __name__ == "__main__":
